13456f8e0cad588c87b867473542d8fc59e780a0
Gupta, Harshit Hemant <hhgupta@iu.edu>
2024-11-16 22:19:30 -0500

    username added

--- controller/search.js
+++ controller/search.js
@@ -1,16 +1,14 @@
 const pool = require('../supaBase');
-
-
 module.exports.getSearch = async (req, res) => {
     try {
         const searchValue = req.params.id;
-
         // Use LOWER() to make the query case-insensitive
         const query = `
             SELECT 
-    employees.employee_id, 
+                employees.employee_id, 
                 employees.first_name, 
                 employees.last_name, 
+                employees.chat_username,
                 employees.title, 
                 employees.phone, 
                 employees.email_id, 
@@ -37,12 +35,12 @@ module.exports.getSearch = async (req, res) => {
         JOIN locations ON employees.location_id = locations.location_id
         JOIN teams ON teams.team_id=employees.team_id
         WHERE LOWER(product_name) LIKE LOWER($1)
-
-        union
-SELECT 
+        UNION
+            SELECT 
                 employees.employee_id, 
                 employees.first_name, 
                 employees.last_name, 
+                employees.chat_username,
                 employees.title, 
                 employees.phone, 
                 employees.email_id, 
@@ -65,15 +63,10 @@ SELECT
                 products.description AS product_description
             FROM 
                 employees
-            JOIN 
-                repositories ON employees.employee_id = repositories.poc_emp_id
-            JOIN 
-                teams ON employees.team_id = teams.team_id
-            JOIN 
-                locations ON employees.location_id = locations.location_id
-            JOIN 
-                products ON repositories.product_id = products.product_id where lower(repository_name) LIKE lower($1) ;
-
+            JOIN repositories ON employees.employee_id = repositories.poc_emp_id
+            JOIN teams ON employees.team_id = teams.team_id
+            JOIN locations ON employees.location_id = locations.location_id
+            JOIN products ON repositories.product_id = products.product_id where lower(repository_name) LIKE lower($1) ;
         `;
 
         const result = await pool.query(query, [`%${searchValue}%`]);
@@ -84,7 +77,7 @@ SELECT
             res.json(result.rows);
         }
     } catch (error) {
-        console.error('Error fetching location:', error.message);
+        console.error('Error fetching repository or product:', error.message);
         res.status(500).send(`Error Message: ${error.message}`);
     }
 };
@@ -97,9 +90,10 @@ module.exports.getSearchByProduct = async (req, res) => {
         // Use LOWER() to make the query case-insensitive
         const query = `
             SELECT 
-    employees.employee_id, 
+                employees.employee_id, 
                 employees.first_name, 
                 employees.last_name, 
+                employees.chat_username, 
                 employees.title, 
                 employees.phone, 
                 employees.email_id, 
@@ -154,6 +148,7 @@ module.exports.getSearchByRepository = async (req, res) => {
                 employees.employee_id, 
                 employees.first_name, 
                 employees.last_name, 
+                employees.chat_username,
                 employees.title, 
                 employees.phone, 
                 employees.email_id, 
