d891db56ff8bd0f24b1b5febf96457e6a4ef8e1c
Nisarg Shah <ns26@iu.edu>
2024-11-16 22:06:54 -0500

    search implemented

--- routes/search.js
+++ routes/search.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const products = require('../controller/search');
 
 
+
+router.get('/product/:id', products.getSearchByProduct);
+
+router.get('/repository/:id', products.getSearchByRepository);
+
 // Route to get a specific location by ID
 router.get('/:id', products.getSearch);
 
